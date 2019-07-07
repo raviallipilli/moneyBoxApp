d219c8aca7e00235f500f948e2cc31744e2b5b62
Ravi Kiran Allipilli <luckyravi17@gmail.com>
2019-07-07 20:34:30 +0100

    Unit tests included

--- src/Moneybox.App/Domain/Account.cs
+++ src/Moneybox.App/Domain/Account.cs
@@ -4,6 +4,27 @@ namespace Moneybox.App
 {
     public class Account
     {
+        private Account() { }
+
+        private readonly string m_customerName;
+
+        private double m_balance;
+
+        private double m_paidIn;
+
+        public const double AccountPayInLimit = 15;
+
+        public Account(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public double AccountBalance
+        {
+            get { return m_balance; }
+        }
+
         public const decimal PayInLimit = 4000m;
 
         public Guid Id { get; set; }
@@ -16,10 +37,22 @@ namespace Moneybox.App
 
         public decimal PaidIn { get; private set; }
 
+        public double AccountPaidIn
+        {
+            get { return m_paidIn; }
+        }
+
         internal bool HasSufficientFundsToWithdraw(decimal amountWithdrawn)
         {
             var balanceLeft = Balance - amountWithdrawn;
             return balanceLeft >= 0m;
+        }
+
+        // testing for sufficient funds
+        public bool HasSufficientFundsToWithdrawTest(double amount)
+        {
+            var balanceLeft = AccountBalance - amount;
+           return balanceLeft >= 0; 
         }
 
         internal void Withdraw(decimal amountWithdrawn)
@@ -28,10 +61,42 @@ namespace Moneybox.App
             Withdrawn = Withdrawn - amountWithdrawn;
         }
 
+        // testing for withdraw
+        public void WithdrawTest(double amountWithdrawn)
+        {
+            if (amountWithdrawn > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amountWithdrawn");
+            }
+
+            if (amountWithdrawn < 0)
+            {
+                throw new ArgumentOutOfRangeException("amountWithdrawn");
+            }
+
+            m_balance -= amountWithdrawn; 
+        }
+
         internal void Deposit(decimal amountDeposited)
         {
             Balance = Balance + amountDeposited;
             PaidIn = PaidIn + amountDeposited;
+        }
+
+        // testing for deposit
+        public void DepositTest(double amountDeposited)
+        {
+            if (amountDeposited > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amountDeposited");
+            }
+
+            if (amountDeposited < 0)
+            {
+                throw new ArgumentOutOfRangeException("amountDeposited");
+            }
+
+            m_balance += amountDeposited; 
         }
 
         internal bool FundsLowAfterWithdrawn(decimal amountWithdrawn, decimal lowLimit)
@@ -40,12 +105,24 @@ namespace Moneybox.App
             return balanceLeft < lowLimit;
         }
 
+        // testing for low sufficient funds
+        public bool FundsLowAfterWithdrawnTest(double amountWithdrawn, double lowLimit)
+        {
+            var balanceLeft = AccountBalance - amountWithdrawn;
+            return balanceLeft < lowLimit;
+        }
+
         internal bool HasPaidInLimitReached(decimal amountToDeposit)
         {
             var balance = PaidIn + amountToDeposit;
-
             return balance > Account.PayInLimit;
         }
 
+        // testing for limit reached funds
+        public bool HasPaidInLimitReachedTest(double amountToDeposit)
+        {
+            var balance = AccountPaidIn + amountToDeposit;
+            return balance > Account.AccountPayInLimit;
+        }
     }
 }
--- src/NUnitTestProject1/UnitTest1.cs
+++ src/NUnitTestProject1/UnitTest1.cs
@@ -1,6 +1,9 @@
 using NUnit.Framework;
 using Moneybox.App.Features;
 using System;
+using Moneybox.App.DataAccess;
+using Moneybox.App.Domain.Services;
+using Moneybox.App;
 
 namespace Tests
 {
@@ -12,9 +15,93 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void _WithdrawTest()
         {
-            Assert.Pass();
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            Account account = new Account("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.WithdrawTest(debitAmount);
+
+            // Assert
+            decimal actual = (decimal)account.AccountBalance;
+            Assert.AreEqual(expected, (double)actual, 0.001, "Account not debited correctly");
+        }
+
+        [Test]
+        public void _DepositTest()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double depositAmount = 4.55;
+            double expected = 16.54;
+            Account account = new Account("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.DepositTest(depositAmount);
+
+            // Assert
+            decimal actual = (decimal)account.AccountBalance;
+            Assert.AreEqual(expected, (double)actual, 16.54, "Account not deposited correctly");
+        }
+
+        [Test]
+        public void _HasSufficientFundsToWithdrawTest()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            Account account = new Account("Mr. Bryan Walton", beginningBalance);
+            var balanceLeft = beginningBalance - debitAmount;
+
+            // Act
+            account.HasSufficientFundsToWithdrawTest(balanceLeft);
+
+            // Assert
+            decimal actual = (decimal)account.AccountBalance;
+            Assert.AreEqual(expected, (double)actual, 7.44, "Account has no suffcient funds to withdraw");
+        }
+
+        [Test]
+        public void _FundsLowAfterWithdrawnTest()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            Account account = new Account("Mr. Bryan Walton", beginningBalance);
+
+            var balanceLeft = beginningBalance - debitAmount;
+           
+            // Act
+            account.FundsLowAfterWithdrawnTest(balanceLeft,0);
+
+            // Assert
+            decimal actual = (decimal)account.AccountBalance;
+            Assert.AreEqual(expected, (double)actual, 7.44, "Account has Funds Low After Withdrawn");
+        }
+
+        [Test]
+        public void _HasPaidInLimitReachedTest()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double depositedAmount = 4.55;
+            double expected = 16.54;
+            Account account = new Account("Mr. Bryan Walton", beginningBalance);
+
+            var balance = beginningBalance + depositedAmount;
+           
+            // Act
+            account.HasPaidInLimitReachedTest(balance);
+
+            // Assert
+            decimal actual = (decimal)account.AccountBalance;
+            Assert.AreEqual(expected, (double)actual, 16.54, "Account has reached its limits");
         }
     }
 }
